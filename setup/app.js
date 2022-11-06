35b4e360302eee4648f26522f9d13df3cc0be014
Alameen8739 <allialameen8739@gmail.com>
2022-11-06 23:40:14 +0100

    placeholder functions

--- setup/app.js
+++ setup/app.js
@@ -5,7 +5,7 @@ const alert = document.querySelector(".alert");
 const grocery = document.getElementById("grocery");
 const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
-const list = documnetg.querySelector(".grocery-list");
+const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
@@ -14,12 +14,65 @@ let editFlag = false;
 let editID = "";
 // ****** EVENT LISTENERS **********
 // submit form
-form.addEventListener("submit", addItem)
+form.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
 function addItem(e) {
     e.preventDefault();
-    console.log(grocery.value);
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if (value !== "" && !editFlag) {
+        const element = document.createElement("article");
+        // add class
+        element.classList.add("grocery-item");
+        // add id
+        const attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+        <!-- edit btn -->
+        <button type="button" class="edit-btn">
+          <i class="fas fa-edit"></i>
+        </button>
+        <!-- delete btn -->
+        <button type="button" class="delete-btn">
+          <i class="fas fa-trash"></i>
+        </button>
+        </div>`;
+
+        // append child
+        list.appendChild(element);
+        // display alert
+        displayAlert("item added to the list", "success");
+        // show container
+        container.classList.add("show-container");
+        // set local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
+    } else {
+        displayAlert("please enter value", "danger")
+    }
 }
+
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert
+    setTimeout(function () {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`)
+    }, 1000)
+}
+
+// set back to default
+function setBackToDefault() {
+    
+}
+
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
